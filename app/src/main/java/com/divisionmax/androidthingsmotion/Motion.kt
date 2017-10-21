4caa13f3653a7c7075275118ff25172244f80198
Gavin Wiener <gavin.w@superbalist.com>
2017-10-21 12:40:09 +0200

    Take a picture when motion is detected

--- app/src/main/java/com/divisionmax/androidthingsmotion/Motion.kt
+++ app/src/main/java/com/divisionmax/androidthingsmotion/Motion.kt
@@ -7,21 +7,26 @@ import android.widget.TextView
 import com.google.android.things.pio.Gpio
 import com.google.android.things.pio.GpioCallback
 import com.google.android.things.pio.PeripheralManagerService
+import CustomCamera
+import android.graphics.Bitmap
+import android.os.Handler
+import android.widget.ImageView
 
 class Motion : Activity() {
 
-    private val tag: String = "MotionActivity"
+    private val TAG: String = "MotionActivity"
     private val MOTION_PIN: String = "GPIO_35"
     private val LED_PIN: String = "GPIO_174"
+
     private var motionSensorGpio: Gpio? = null
     private var ledGpio: Gpio? = null
-    private var txtValue : TextView? = null
+    private var imageView: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_motion)
-        txtValue = findViewById(R.id.txtvw_value)
-
+        initCamera()
+        imageView = findViewById(R.id.img_view)
         motionSensorGpio = PeripheralManagerService().openGpio(MOTION_PIN)
         ledGpio = PeripheralManagerService().openGpio(LED_PIN)
         ledGpio?.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
@@ -30,9 +35,9 @@ class Motion : Activity() {
         flashLed()
 
         if (motionSensorGpio == null){
-            Log.d(tag, "No motion sensor available")
+            Log.d(TAG, "No motion sensor available")
         }else {
-            Log.d(tag, "There is a motion sensor")
+            Log.d(TAG, "There is a motion sensor")
         }
         motionSensorGpio?.setDirection(Gpio.DIRECTION_IN)
         motionSensorGpio?.setActiveType(Gpio.ACTIVE_HIGH)
@@ -40,11 +45,10 @@ class Motion : Activity() {
         motionSensorGpio?.registerGpioCallback(object : GpioCallback() {
             override fun onGpioEdge(gpio: Gpio): Boolean {
                 if (gpio.value) {
-                    Log.i(tag, String.format("Sensor value, %s", gpio.value))
-                    txtValue?.setText("Keep movin'!")
+                    Log.i(TAG, String.format("Sensor value, %s", gpio.value))
+                    camera?.takePicture()
                 } else {
-                    Log.i(tag, "No sensor value available")
-                    txtValue?.setText("...")
+                    Log.i(TAG, "No sensor value available")
                 }
 
                 return true
@@ -52,12 +56,29 @@ class Motion : Activity() {
         })
     }
 
+    private var camera: CustomCamera? = null
+
+    private fun initCamera() {
+        camera = CustomCamera.getInstance()
+        if (camera == null){
+            Log.d(TAG, "Camera not available")
+        }
+        camera?.initializeCamera(this, Handler(), imageAvailableListener)
+    }
+
+    private val imageAvailableListener = object: CustomCamera.ImageCapturedListener {
+        override fun onImageCaptured(bitmap: Bitmap) {
+            imageView?.setImageBitmap(bitmap)
+            //motionViewModel.uploadMotionImgae(bitmap)
+        }
+    }
+
     private fun flashLed() {
-        txtValue?.postDelayed({
-            val b = ledGpio?.value;
-            ledGpio?.value = !b!!
-            flashLed()
-        }, 1000)
+//        txtValue?.postDelayed({
+//            val b = ledGpio?.value;
+//            ledGpio?.value = !b!!
+//            flashLed()
+//        }, 1000)
     }
 
     override fun onDestroy() {
